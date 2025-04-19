78e05daf1c4d2e188b5670140573e247a92b054b
itzfahadoooo <foyindolapo@gmail.com>
2025-04-19 14:58:55 +0100

    Initial Commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -61,6 +61,7 @@ export default function Navbar() {
                 Healthcare
               </Link>
             </DropdownMenuItem>
+            
             <DropdownMenuItem>
               <Link to="/industries/finance" className="w-full">
                 Finance
