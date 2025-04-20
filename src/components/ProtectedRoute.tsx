b48184ab2b64dfb6a24395b47b7b4d4f36c4a946
itzfahadoooo <foyindolapo@gmail.com>
2025-04-20 19:16:29 +0100

    Initial Commit

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -5,6 +5,7 @@ import { Navigate } from "react-router-dom";
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
   const { user, loading } = useAuth();
 
+  
   if (loading) return <p>Loading...</p>;
   if (!user) return <Navigate to="/login" />;
 
