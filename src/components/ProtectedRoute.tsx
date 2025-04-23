956ec199d3e04b2fb8a7af5c3c28682bee407e40
itzfahadoooo <foyindolapo@gmail.com>
2025-04-23 14:44:27 +0100

    Initial Commit

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,7 +1,6 @@
-// src/components/ProtectedRoute.tsx
 import { useAuth } from "@/contexts/AuthContext";
 import { Navigate } from "react-router-dom";
-import { BallTriangle } from "react-loader-spinner";
+import { DotLoader } from "react-spinners";
 
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
   const { user, loading } = useAuth();
@@ -9,7 +8,7 @@ const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
   if (loading)
     return (
       <div className="flex items-center justify-center h-screen">
-        <BallTriangle height={100} width={100} color="#008000" ariaLabel="Loading"/>
+        <DotLoader color="#008000" size={60}  />
       </div>
     );
 
