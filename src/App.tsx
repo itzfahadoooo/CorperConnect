6b950233f22f4e9f60318a796f9a3594ccc14f76
itzfahadoooo <foyindolapo@gmail.com>
2025-04-23 14:27:56 +0100

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,9 @@ export default function App() {
   return (
 
     <Routes>
-      <Route path="/" element={<Home />} />
       
+      <Route path="/" element={<Home />} />
+
 
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
