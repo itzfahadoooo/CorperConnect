a7f51f8b631db7c0ba1844d6b18213afdcdddb3b
itzfahadoooo <foyindolapo@gmail.com>
2025-04-23 23:54:00 +0100

    Initial Commit

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,6 +1,6 @@
 import { useAuth } from "@/contexts/AuthContext";
 import { Navigate } from "react-router-dom";
-import { DotLoader } from "react-spinners";
+import { Audio } from "react-loader-spinner";
 
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
   const { user, loading } = useAuth();
@@ -8,7 +8,15 @@ const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
   if (loading)
     return (
       <div className="flex items-center justify-center h-screen">
-        <DotLoader color="#008000" size={60}  />
+        <Audio
+  height="100"
+  width="100"
+  color="#4fa94d"
+  ariaLabel="audio-loading"
+  wrapperStyle={{}}
+  wrapperClass="wrapper-class"
+  visible={true}
+  />
       </div>
     );
 
