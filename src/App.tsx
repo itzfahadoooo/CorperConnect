8f361ad3c51164ef1866dcdecb56c859fa761d98
itzfahadoooo <foyindolapo@gmail.com>
2025-04-23 14:56:40 +0100

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Register";
@@ -8,24 +8,26 @@ import ResetPassword from "./pages/ResetPassword";
 
 export default function App() {
   return (
+    <BrowserRouter>
+    
+      <Routes>
 
-    <Routes>
-      
-      <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
 
 
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/resetpassword" element={<ResetPassword />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/resetpassword" element={<ResetPassword />} />
 
-      <Route
-        path="/dashboard"
-        element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        }
-      />
-    </Routes>
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
