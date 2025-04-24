d7876738896936aa6a473df4bdc7ff37c3dae05f
itzfahadoooo <foyindolapo@gmail.com>
2025-04-24 09:51:21 +0100

    Initial Commit

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -5,6 +5,7 @@ import { DotLoader } from "react-spinners";
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
   const { user, loading } = useAuth();
 
+  
   if (loading)
     return (
       <div className="flex items-center justify-center h-screen">
