1cc4b40dc9f6d7c8a60be45d35443a818c8fece4
itzfahadoooo <foyindolapo@gmail.com>
2025-05-07 20:13:32 +0100

    Initial Commit

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -179,6 +179,7 @@ const Dashboard = () => {
               </a>
 
             </div>
+            
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
               {housingListings.map((listing) => (
