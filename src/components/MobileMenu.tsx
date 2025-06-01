2de9c6869f5a86b979f282a653143936427c94a6
itzfahadoooo <foyindolapo@gmail.com>
2025-06-01 19:58:48 +0100

    Initial Commit

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,11 +1,6 @@
 import { useState } from "react";
-
 import { Link } from "react-scroll";
-
-
 import { Button } from "./ui/button";
-
-
 import { Link as RouterLink } from "react-router-dom";
 
 const MobileMenu = () => {
