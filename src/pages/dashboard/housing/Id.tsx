da8c3c215904280200e2f8bded1cbe1d2c3a6292
itzfahadoooo <foyindolapo@gmail.com>
2025-06-05 19:55:02 +0100

    Initial Commit

--- src/pages/dashboard/housing/Id.tsx
+++ src/pages/dashboard/housing/Id.tsx
@@ -9,6 +9,7 @@ import { useParams } from "react-router-dom";
 
 import { Link } from "react-router-dom";
 
+
 export default function HousingDetailPage() {
   const params = useParams();
   const id = params.id;
