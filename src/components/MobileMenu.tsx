f3e208f925c112d7db5ee749693c0996c2556839
Fahad Idrees Oyindolapo <foyindolapo@gmail.com>
2025-06-22 09:59:44 +0100

    Initial commit

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -24,13 +24,14 @@ const MobileMenu = () => {
           strokeLinecap="round"
           strokeLinejoin="round"
           strokeWidth="2"
-          
+
           viewBox="0 0 24 24"
 
           stroke="currentColor"
         >
           {isOpen ? (
             <path d="M6 18L18 6M6 6l12 12" />
+            
           ) : (
 
             <path d="M4 6h16M4 12h16M4 18h16" />
