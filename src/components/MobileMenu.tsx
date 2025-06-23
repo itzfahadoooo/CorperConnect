a1601ee4501dc875a7977f571356189a6d52fe37
Fahad Idrees Oyindolapo <foyindolapo@gmail.com>
2025-06-23 21:24:14 +0100

    Initial commit

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -38,10 +38,11 @@ const MobileMenu = () => {
 
             <path d="M4 6h16M4 12h16M4 18h16" />
             
+            
           )}
 
         </svg>
-        
+
       </button>{" "}
       
 
