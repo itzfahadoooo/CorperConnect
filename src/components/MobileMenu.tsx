f58ba341fa6adf1f562403bc65261a4272fe0679
Fahad Idrees Oyindolapo <foyindolapo@gmail.com>
2025-06-23 21:23:45 +0100

    Initial commit

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -27,7 +27,7 @@ const MobileMenu = () => {
 
           viewBox="0 0 24 24"
 
-          
+
           stroke="currentColor"
         >
           {isOpen ? (
@@ -35,6 +35,7 @@ const MobileMenu = () => {
 
           ) : (
 
+            
             <path d="M4 6h16M4 12h16M4 18h16" />
             
           )}
