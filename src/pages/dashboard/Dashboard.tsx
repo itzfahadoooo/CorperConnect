e05cbde08c3ee867b232f8d6128027e9abdf2766
Fahad Idrees Oyindolapo <foyindolapo@gmail.com>
2025-07-07 18:05:15 +0100

    Initial commit

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -31,6 +31,7 @@ const Dashboard = () => {
       id: "1",
       title: "Getting Around in Lagos",
       
+      
       author: "Chioma Okafor",
       content:
         "Use the BRT buses for longer distances - they're cheaper and avoid traffic with dedicated lanes.",
